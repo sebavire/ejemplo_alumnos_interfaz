e5bc3140dd60f6f5f950eb6819a3bfa4e369d6cb
sebavire <129177017+sebavire@users.noreply.github.com>
2025-08-28 10:20:34 -0300

    look and feel

--- src/ejemplo_alumnos/Ejemplo_alumnos.java
+++ src/ejemplo_alumnos/Ejemplo_alumnos.java
@@ -3,15 +3,40 @@ package ejemplo_alumnos;
 import Dominio.principal;
 import Presentacion.formularioAlumnos;
 import Presentacion.formularioInscripcion;
+import javax.swing.UIManager;
 
 public class Ejemplo_alumnos {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatIntelliJLaf");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         principal principal = new principal();
         formularioAlumnos form = new formularioAlumnos(principal);
-        formularioInscripcion formInscripcion = new formularioInscripcion(principal);
         //form.setVisible(true);
+        
+        formularioInscripcion formInscripcion = new formularioInscripcion(principal);
         formInscripcion.setVisible(true);
+
+        /*try {
+
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+
+            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatLightLaf");   
+            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatDarkLaf");    
+            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatIntelliJLaf");
+            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatDarculaLaf"); 
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }*/
+
     }
-    
+
 }
